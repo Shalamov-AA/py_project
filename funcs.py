62186260caaf377cf90ce8be7b4286fa3815d6c6
shalam <shlmv92@gmail.com>
2023-05-08 19:36:22 +0300

    Added function substraction

--- funcs.py
+++ funcs.py
@@ -7,3 +7,14 @@ def summa():
     print(f"Сумма чисел: {a+b}")
 
 summa()
+
+def subtraction():
+    print("Введите первое число:")
+    a = int(input())
+    print("Введите первое число:")
+    b = int(input())
+
+    print(f"Разность чисел: {a - b}")
+
+
+subtraction()
